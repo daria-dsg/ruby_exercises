e3c7fa7a21bdfb779960388ba00dd6bc07183ff6
Daria <daria.ui.ux@gmail.com>
2022-11-23 14:31:02 +0100

    change the test for move since moving stratgey was reconsider

--- hanoi/spec/hanoi_spec.rb
+++ hanoi/spec/hanoi_spec.rb
@@ -10,17 +10,22 @@ describe HanoiTower do
     end
    
     it "moves disque from one pile to other" do 
-      expect(hanoi.board[[1,0]]).to eq(1)
+      expect(hanoi.board[[1,3]]).to eq(1)
       expect(hanoi.board[[0,0]]).to eq(nil)
     end
 
     it "raise en error when start pile is empty or not valid" do
        expect{ hanoi.move(2,1) }.to raise_error(RuntimeError)
-       expect{ hanoi.move(3,1) }.to raise_error(RuntimeError)
     end
 
-    # it "raise en error when end pile has the smaller disque on the bottom" do 
+    it "raise en error when end pile has the smaller disque on the bottom " do 
+      
+    end
 
+    # it "moves only disques" do
+    #   hanoi.move(0,2)
+    #   expect(hanoi.piles).not_to eq (hanoi.piles)
+    #   expect(hanoi.piles).to eq([[nil, nil, 3, 4], [nil, nil, nil, nil], [nil, nil, nil, nil]])
     # end
   end
 end
