672efd99b5d874a280921736105ddc2453b1bbd2
Daria <daria.ui.ux@gmail.com>
2022-11-23 12:18:25 +0100

    refactor and debug move method

---
+++ hanoi/lib/hanoi.rb
@@ -0,0 +1,27 @@
+require_relative 'board'
+require 'byebug'
+
+class HanoiTower
+  attr_reader :board
+
+  def initialize(size)
+    @board = Board.new(size)
+  end
+
+  def move(start_pile, end_pile)
+    disque = board[[start_pile,0]]
+    raise "an empty pile" if disque == nil
+    
+    board[[start_pile,0]], board[[end_pile, 0]] = nil, disque
+  end
+
+  def play
+    print "Enter the start pile: "
+    start_pile = gets.chomp.to_i - 1
+
+    print "Enter the end pile: "
+    end_pile = gets.chomp.to_i - 1
+    move(start_pile, end_pile)
+  end
+
+end
