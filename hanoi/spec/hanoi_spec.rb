a2be99b7b90a39d8b5a4d14dc3bb5738db03dd23
Daria <daria.ui.ux@gmail.com>
2022-11-24 12:52:04 +0100

    change the before each #move arguments for tesring purpose

--- hanoi/spec/hanoi_spec.rb
+++ hanoi/spec/hanoi_spec.rb
@@ -6,23 +6,29 @@ describe HanoiTower do
    
   describe "#move" do
     before(:each) do
-      hanoi.move(0,1)
+      hanoi.move(0,2)
     end
    
     it "moves disque from one pile to other" do 
-      expect(hanoi.board[1][0]).to eq(1)
+      expect(hanoi.board[2][0]).to eq(1)
       expect(hanoi.board[0][0]).to eq(2)
     end
 
     it "raises en exception when start pile is empty" do
-       expect{ hanoi.move(2,1) }.to raise_error(RuntimeError)
+       expect{ hanoi.move(1,2) }.to raise_error(RuntimeError)
     end
 
-    it "raises an exception when start or ending piles are not valid" do 
-    end
-
-    it "raises an excpetion when end pile has the smaller disque on the bottom " do 
-      expect{ hanoi.move(0,1) }.to raise_error(RuntimeError)
+    it "raises an exception when end pile has the smaller disque on the bottom " do 
+      expect{ hanoi.move(0,2) }.to raise_error(RuntimeError)
     end
   end
+
+  # describe "#won" do 
+  #   it "returns true when all disque at the last pile" do
+  #     expect(hanoi.move).
+  #   end
+
+  #   it "returns false when not all disque at the last pile" do 
+  #   end
+  # end
 end
