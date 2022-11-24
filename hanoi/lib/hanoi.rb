774de73d76eb37fc4cc6cdd7e0d7825352988c5c
Daria <daria.ui.ux@gmail.com>
2022-11-24 07:20:08 +0100

    add prompt method to refactor

--- hanoi/lib/hanoi.rb
+++ hanoi/lib/hanoi.rb
@@ -1,41 +1,23 @@
 require_relative 'board'
-require 'byebug'
 
 class HanoiTower
-  attr_reader :board, :size
+  attr_reader :board
 
   def initialize(size)
     @board = Board.new(size)
-    @size = size
-    @piles = board.piles
   end
 
-  # def move(start_pile, end_pile)
-  #   disque = board[[start_pile,0]]
-  #   raise "an empty pile" if disque == nil
-    
-  #   board[[start_pile,0]], board[[end_pile, size - 1]] = nil, disque
-  # end
-
   def move(start_pile, end_pile)
-    piles.each do |pile|
-      next if disque == nil
-      
-      disque = board[[start_pile,0]]
-      board[[start_pile,0]], board[[end_pile, size - 1]] = nil, disque
-      return
-    end
 
-    raise "an empty pile"
+  end
+
+  def prompt(pile)
+    print "Enter the #{pile} pile: "
+    gets.chomp.to_i - 1
   end
 
   def play
-    print "Enter the start pile: "
-    start_pile = gets.chomp.to_i - 1
-
-    print "Enter the end pile: "
-    end_pile = gets.chomp.to_i - 1
+    start_pile, end_pile = prompt("starting"), prompt("ending")
     move(start_pile, end_pile)
   end
-
 end
